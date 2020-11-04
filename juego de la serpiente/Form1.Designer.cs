fea95398f348065ec8eb094f54967a58dd225db5
IngMauVega <73960971+IngMauVega@users.noreply.github.com>
2020-11-04 13:06:55 -0600

    Add files via upload

---
+++ juego de la serpiente/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace juego_de_la_serpiente
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblpuntos = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblpuntos
+            // 
+            this.lblpuntos.AutoSize = true;
+            this.lblpuntos.BackColor = System.Drawing.Color.Transparent;
+            this.lblpuntos.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblpuntos.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpuntos.Location = new System.Drawing.Point(549, 232);
+            this.lblpuntos.Name = "lblpuntos";
+            this.lblpuntos.Size = new System.Drawing.Size(30, 31);
+            this.lblpuntos.TabIndex = 1;
+            this.lblpuntos.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::juego_de_la_serpiente.Properties.Resources.tablero;
+            this.ClientSize = new System.Drawing.Size(603, 446);
+            this.Controls.Add(this.lblpuntos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MoverPieza);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblpuntos;
+    }
+}
+
---
+++ juego de la serpiente/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace juego_de_la_serpiente
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> Lista = new List<PictureBox>();
+        int tamanoPiezaPrincipal = 80, tiempo = 10;
+        PictureBox comida = new PictureBox();
+        string direccion = "right";
+       
+        public Form1()
+        {
+            InitializeComponent();
+            IniciarJuego();
+            
+        }
+        public void IniciarJuego()
+        {
+            tiempo = 10;
+            direccion = "right";
+            timer1.Interval = 200;
+            Lista = new List<PictureBox>();
+            lblpuntos.Text = "0";
+            //piezas iniciales
+            for(int i=2;0<=i;i=i-1)
+            {
+                Crearchuek(Lista, this, (i * tamanoPiezaPrincipal) + 70, 80);
+            }
+            CrearDinero();
+
+        }
+        public void Crearchuek(List<PictureBox> Listapelota, Form formulario, int posicionX, int posicionY)
+        {
+            PictureBox pb = new PictureBox();
+            pb.Location = new Point(posicionX, posicionY);
+           // pb.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("Shrek");
+            pb.Image = Image.FromFile(Application.StartupPath + (@"\img\Shrek.png"));
+            pb.BackColor = Color.Transparent;
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;
+            Listapelota.Add(pb);
+            formulario.Controls.Add(pb);
+        }
+        public void  CrearDinero()
+        {
+            Random rdn = new Random();
+            int enterox = rdn.Next(1, this.Width - tamanoPiezaPrincipal - 10);
+            int enteroy = rdn.Next(1, this.Height - tamanoPiezaPrincipal - 40);
+
+            PictureBox pb = new PictureBox();
+            pb.Location = new  Point (enterox,enteroy);
+            pb.Image=Image.FromFile(Application.StartupPath + (@"\img\dinero.jpg"));
+            //  pb.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("dinero2");
+            pb.BackColor = Color.Transparent;
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;
+            comida = pb;
+            this.Controls.Add(pb);
+
+
+        }
+
+        private void MoverPieza(object sender, KeyEventArgs e)
+        {
+            //depende que toque es pa donde va ir
+            direccion = ((e.KeyCode & Keys.Up)==Keys.Up) ? "up" : direccion;
+            direccion = ((e.KeyCode & Keys.Down)==Keys.Down) ? "down" : direccion;
+            direccion = ((e.KeyCode & Keys.Up)==Keys.Left) ? "left" : direccion;
+            direccion = ((e.KeyCode & Keys.Up)==Keys.Right) ? "right" : direccion;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int nx = Lista[0].Location.X;
+            int ny = Lista[0].Location.Y;
+            //colocar a tomas
+            Lista[0].Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("tomas" + direccion);
+            for(int i=Lista.Count-1;i>=0; i=i-1)
+            {
+                if(i==0)
+                {
+                    if (direccion == "right") nx = nx + tamanoPiezaPrincipal;
+                    else if (direccion == "left") nx = nx - tamanoPiezaPrincipal;
+                    else if (direccion == "up") nx = nx - tamanoPiezaPrincipal;
+                    else if (direccion == "down") nx = nx + tamanoPiezaPrincipal;
+                 //   Lista[0].Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("tomas"+direccion) ;
+                    Lista[0].Location = new Point(nx, ny);
+                }
+                else
+                {
+                    //intercambio de seguimiento
+                    Lista[i].Location = new Point((Lista[i - 1].Location.X), (Lista[i].Location.Y));
+                    Lista[i].Location = new Point((Lista[i].Location.X), (Lista[i-1].Location.Y));
+
+                }
+                //for (int contarpiezas = 1; contarpiezas < Lista.Count; contarpiezas = i + 1)
+                //{
+                //    if (Lista[contarpiezas].Bounds.IntersectsWith(comida.Bounds))
+                //    {
+                //        this.Controls.Remove((comida));//desaparecer el dinero
+                //        tiempo = Convert.ToInt32(timer1.Interval);//mas tiempo
+                //        if (tiempo > 10)
+                //        {
+                //            timer1.Interval = tiempo - 10;
+                //        }
+                //        lblpuntos.Text = (Convert.ToInt32(lblpuntos.Text) + 1).ToString();
+                //       // CrearDinero();
+                //      //  Crearchuek(Lista, this, Lista[Lista.Count - 1].Location.X * tamanoPiezaPrincipal, 0);//nuevo chuek
+                //    }
+                //}
+            }
+        }
+    }
+}
